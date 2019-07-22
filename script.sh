9c7878b4313965c99a8c7068dbc3768cffe06efb
Soohwan Hyun <xjavalov@shhyun.com>
2019-07-22 13:20:56 +0900

    you complete me addon compiling added

--- script.sh
+++ script.sh
@@ -1,6 +1,8 @@
 sudo apt update -y
-sudo apt install vim -y
+sudo apt install vim build-essential cmake python3-dev -y
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp .vimrc ~/
+cd ~/.vim/bundle/YouCompleteMe
+python3 install.py --clang-completer
 vim +PluginInstall +qall
 
